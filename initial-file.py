4507e9ab24dec44bd602086cabe6b32720d83715
Griselle Jimenez Ruiz <griselle.jr@gmail.com>
2021-08-19 22:20:20 -0500

    added switch case for categories

--- initial-file.py
+++ initial-file.py
@@ -19,10 +19,58 @@ def main():
 
     api_url_cat = "https://quotes.rest/qod/categories?language=en&detailed=false"
     response = requests.get(api_url_cat)
-    print(response.json()['contents'])
+
+    list_of_cats = response.json()['contents']['categories']
+
+    print('Let\'s see what tickles your fancy')
+    time.sleep(1)
+    print('Choose one of the following...')
+    for category in list_of_cats:
+        print(category.capitalize())
+        time.sleep(0.5)
     
-    for category in response.json()['contents']['categories']:
-        print(category)
+    chosen_cat = input("What do you choose?  ")
+
+    print("Ok, just a second....(imagine Jeopardy song plaing, R.I.P. Alex)")
+    time.sleep(1.5)
+
+    message = ''
+    author = ''
+
+    if chosen_cat.upper() == "INSPIRE":
+        quote_response = requests.get("https://quotes.rest/qod?category=inspire&language=en")
+        message = quote_response.json()['contents']['quotes'][0].quote
+        author = message = quote_response.json()['contents']['quotes'][0].author
+    elif chosen_cat.upper() == "MANAGEMENT":
+        quote_response = requests.get("https://quotes.rest/qod?category=management&language=en")
+        message = quote_response.json()['contents']['quotes'][0].quote
+        author = message = quote_response.json()['contents']['quotes'][0].author
+    elif chosen_cat.upper() == "SPORTS":
+        quote_response = requests.get("https://quotes.rest/qod?category=sports&language=en")
+        message = quote_response.json()['contents']['quotes'][0].quote
+        author = message = quote_response.json()['contents']['quotes'][0].author
+    elif chosen_cat.upper() == "LIFE":
+        quote_response = requests.get("https://quotes.rest/qod?category=life&language=en")
+        message = quote_response.json()['contents']['quotes'][0].quote
+        author = message = quote_response.json()['contents']['quotes'][0].author
+    elif chosen_cat.upper() == "FUNNY":
+        quote_response = requests.get("https://quotes.rest/qod?category=funny&language=en")
+        message = quote_response.json()['contents']['quotes'][0].quote
+        author = message = quote_response.json()['contents']['quotes'][0].author
+    elif chosen_cat.upper() == "LOVE":
+        quote_response = requests.get("https://quotes.rest/qod?category=love&language=en")
+        message = quote_response.json()['contents']['quotes'][0].quote
+        author = message = quote_response.json()['contents']['quotes'][0].author
+    elif chosen_cat.upper() == "ART":
+        quote_response = requests.get("https://quotes.rest/qod?category=art&language=en")
+        message = quote_response.json()['contents']['quotes'][0].quote
+        author = message = quote_response.json()['contents']['quotes'][0].author
+    elif chosen_cat.upper() == "STUDENTS":
+        quote_response = requests.get("https://quotes.rest/qod?category=students&language=en")
+        message = quote_response.json()['contents']['quotes'][0].quote
+        author = message = quote_response.json()['contents']['quotes'][0].author
+    
+    print(message + "by " + author)
 
 if __name__ == "__main__":
     main()
